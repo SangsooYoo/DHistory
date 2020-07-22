dea32c182e23976add35f6ab7bcfa713dbbe1772
Sangsoo Ryan Yoo <ssyoo@humaxdigital.com>
2020-07-22 11:29:42 +0900

    testcase update

--- app/src/main/java/com/evadummy/codingtest/MainActivity.java
+++ app/src/main/java/com/evadummy/codingtest/MainActivity.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.humaxdigital.automotive.codingtest.testcases.SolutionL3012;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< HEAD:app/src/main/java/com/evadummy/codingtest/MainActivity.java
 
 
+=======
+        SolutionL3012 solutionL30002 = new SolutionL3012();
+        solutionL30002.solution(3,5);
+>>>>>>> testcase update:app/src/main/java/com/humaxdigital/automotive/codingtest/MainActivity.java
     }
 }
